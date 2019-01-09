6d44e7232c801318983766512b5024d8065b0c19
Maria Shklovskaya <mariashklovskaya@coherentsolutions.com>
2019-01-09 17:39:52 +0300

    feat: add mongoose config and connect

--- Devconnector-project/server.js
+++ Devconnector-project/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello Mary'));
+// DB Config
+const db = require('./config/keys').mongoURI;
 
-const port = process.env.PORT || 5000;
+// Connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello Mary!!! '));
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
